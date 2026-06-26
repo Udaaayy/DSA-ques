6f062d89888df5c979d43f6edf67f2b62b465c1d
Udaaayy <udaysharma7566@gmail.com>
2026-06-26 13:38:53 +0530

    linear_seach_algorithm

---
+++ array/linearsearch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int linearsearch(int arr[],int sz ,int target )
+  {
+    for (int i =0; i<sz;i++){
+        if (arr[i]==target)
+        {
+            return i;
+        }
+        
+    }
+      return -1;
+  }
+
+int main (){
+    int arr[]={1,2,4,5,6,7,9};
+    int sz = 7;
+  int  target=9;
+  cout<< linearsearch(arr,sz,target)<<endl;
+return 0;
+}
