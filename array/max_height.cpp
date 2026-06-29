8d2d8ba5ceb8c528e492bd1c0b7d92906e7871f5
Udaaayy <udaysharma7566@gmail.com>
2026-06-29 22:32:57 +0530

    max water container question

---
+++ array/max_height.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[]={1,8,6,2,5,4,8,3,9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxwater=0;
+    for (int i = 0; i < n ; i++)
+    {
+        for (int j = i+1 ; j < n ; j++)
+        {
+           int w= j-1;
+           int ht= min(arr[i],arr[j]) ;
+           int area=w*ht;
+           maxwater=max(maxwater,area); 
+        }
+        
+    }
+    cout << "Max water: " << maxwater << endl;
+
+    return 0;
+}
