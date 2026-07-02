563c121320b8f4e92d88dd2c492f824dc30d3938
Udaaayy <udaysharma7566@gmail.com>
2026-07-02 14:09:10 +0530

    binary search

---
+++ array/binary_search/binary_search.cpp
@@ -0,0 +1,35 @@
+//iterative method for binary search//
+#include<iostream>
+#include<vector>
+using namespace std;
+int binarysearch(vector<int>arr , int tar){
+int st =0, end=arr.size()-1;
+ 
+while(st <= end){
+    int mid = (st + end)/2;
+
+    if( tar > arr[mid]){
+          st = mid +1;
+    }
+    else if ( tar > arr [mid]){
+           end =mid -1;   
+    }
+    else {
+        return mid ;
+    }
+}
+    return -1;
+}
+int main(){
+vector<int> arr1 ={-1,0,2,3,4,11,12}; //odd
+int tar1 =12;
+
+cout<< binarysearch(arr1,tar1)<< endl;
+
+vector<int> arr2 = {-1,0,4,6,8,10,11}; //even
+int tar2 =0;
+
+cout<< binarysearch(arr2,tar2)<< endl;
+
+return 0;
+}
