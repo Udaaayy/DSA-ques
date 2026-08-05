5ceb822c3527ab55e65e6435d98b7e2ecd931fdb
Udaaayy <udaysharma7566@gmail.com>
2026-08-05 19:26:22 +0530

    strings started

---
+++ strings/reverse_string.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void reversestring( string& s){
+     int st=0 , end =s.size()-1;
+
+        while(st < end){
+            swap(s[st++] , s[end--] );
+        }
+}
+int main(){
+
+std::string userInput;
+
+    std::cout << "Enter a string: ";
+    // Use std::getline to capture full lines including spaces
+    std::getline(std::cin, userInput);
+;
+    
+    reversestring(userInput);
+cout << userInput;
+
+    return 0;
+}
